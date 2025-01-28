03e4271c2739f0720dba37afdcd5a36d578e4223
Pascal Nitcheu <nitcheupascal@gmail.com>
2025-01-28 15:24:33 +0100

    Project Liste Screen component established

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClientProvider } from 'react-query';
 import queryClient from './src/utils/queryClient';
-import ProjectListScreen from './src/screens/ProjectListScreen';
+import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <ProjectListScreen />
+        <RootNavigator /> 
       </NavigationContainer>
     </QueryClientProvider>
   );
